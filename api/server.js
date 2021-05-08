0cd99630ba1e5aded7fad1c2904cc9f816269a9b
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-07 22:57:11 -0400

    GET enpoints / and /:id working correctly

--- api/server.js
+++ api/server.js
@@ -4,6 +4,13 @@ const postsRouter = require("./posts/posts-router");
 const server = express();
 
 server.use(express.json());
-server.use(postsRouter);
+server.use("/api/posts", postsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>Lambda Blog API</h2>
+    <p> Welcome to the Lambda Blog API</p>
+    `);
+});
 
 module.exports = server;
