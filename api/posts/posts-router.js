e181bf41ffff48258218538fd6bd933efff941fe
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-07 23:19:48 -0400

    DELETE enpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -87,7 +87,28 @@ router.put("/:id", (req, res) => {
   }
 });
 
-router.delete;
+router.delete("/:id", (req, res) => {
+  posts.findById(req.params.id).then((post) => {
+    if (post) {
+      posts
+        .remove(req.params.id)
+        .then((count) => {
+          if (count) {
+            res.status(200).json(post);
+          }
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: "The post could not be removed",
+          });
+        });
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    }
+  });
+});
 
 router.get;
 
